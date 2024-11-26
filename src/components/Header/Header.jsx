deefd6d6274dc25fb238edd07aad5c64807e8f43
Fabian Guerrero <guerrerofabian1985@gmail.com>
2024-11-26 15:58:20 +0100

    Update header Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,13 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header-wrapper">
-      <Link to={"/"} className="home-link" aria-label="Go to Homepage">
+      <Link
+        to={
+          "https://fabian-guerrero.github.io/devchallenges-CountryPageWorldRanks/"
+        }
+        className="home-link"
+        aria-label="Go to Homepage"
+      >
         <img className="page-logo" src={pageLogo} alt="World Rank Logo" />
       </Link>
     </div>
