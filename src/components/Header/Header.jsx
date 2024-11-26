015e94ba0b01756ff7b3e12007a1b1f3af7d1e6b
Fabian Guerrero <guerrerofabian1985@gmail.com>
2024-11-26 16:17:56 +0100

    Fix header Link path

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header-wrapper">
-      <Link
-        to={
-          "https://fabian-guerrero.github.io/devchallenges-CountryPageWorldRanks/"
-        }
-        className="home-link"
-        aria-label="Go to Homepage"
-      >
+      <Link to={"/"} className="home-link" aria-label="Go to Homepage">
         <img className="page-logo" src={pageLogo} alt="World Rank Logo" />
       </Link>
     </div>
